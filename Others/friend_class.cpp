ab37de9cfe638c521e9f3ee8ad4f41d4ae6453c6
shrikardongre <shrikardongre@gmail.com>
2025-10-04 10:53:48 +0530

    friend implementation

--- Others/friend_class.cpp
+++ Others/friend_class.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
 #include <string>
+using namespace std ;
 
-// We need to forward-declare the Government class because President uses it.
-class Government; 
+class president ;
 
-class President {
-public:
-    void launchNukes(Government& gov); // Takes a Government object
+class government {
+  private:
+   string code ="123";
+   public :
+   friend class president ;
 };
 
-class Government {
-private:
-    std::string nuclearLaunchCode = "BOOM123";
+class president {
+    public :
+    void getcode (government &gov) {
+        cout<< "the secret code is " << gov.code <<endl;
+    }
 
-public:
-    // Granting friendship to the ENTIRE President class.
-    // Every member function of President can now access our private members.
-    friend class President; 
 };
 
-// Now we define the President's member function.
-void President::launchNukes(Government& gov) {
-    std::cout << "Accessing launch codes..." << std::endl;
-    std::cout << "Code: " << gov.nuclearLaunchCode << std::endl; // Access is allowed!
-    // This works only because President is a friend of Government.
-}
-
-int main() {
-    Government usa;
-    President mr_president;
-
-    mr_president.launchNukes(usa); 
-    // Output:
-    // Accessing launch codes...
-    // Code: BOOM123
-
-    return 0;
+int main(){
+    government gov ;
+    president p;
+    p.getcode(gov) ;
 }
